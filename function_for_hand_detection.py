e7444f0179646d91bf461384d943e9ffd0880759
Jaison Jose <jaisonjose241@gmail.com>
2020-06-02 16:59:10 +0530

    third commit

--- function_for_hand_detection.py
+++ function_for_hand_detection.py
@@ -30,20 +30,27 @@ image = 0
 font = cv2.FONT_HERSHEY_PLAIN
 color = [(0, 255, 255), (255, 255, 0), (0, 255, 0)]                              #yellow, blue, green
 mode = ''
+trainCount = {}
+testCount = {}
+if not os.path.exists("data"):
+        os.makedirs("data")
+        os.makedirs("data/TRAIN")
+        os.makedirs("data/TEST")
+        
+        os.makedirs("data/TRAIN/0")
+        os.makedirs("data/TRAIN/1")
+        os.makedirs("data/TRAIN/2")
+        os.makedirs("data/TRAIN/3")
+        os.makedirs("data/TRAIN/4")
+        os.makedirs("data/TRAIN/5")
+        
+        os.makedirs("data/TEST/0")
+        os.makedirs("data/TEST/1")
+        os.makedirs("data/TEST/2")
+        os.makedirs("data/TEST/3")
+        os.makedirs("data/TEST/4")
+        os.makedirs("data/TEST/5")
 
-trainCount = {'zero': len(os.listdir("data/train/0")),
-             'one':   len(os.listdir("data/train/1")),
-             'two':   len(os.listdir("data/train/2")),
-             'three': len(os.listdir("data/train/3")),
-             'four':  len(os.listdir("data/train/4")),
-             'five':  len(os.listdir("data/train/5"))}
-
-testCount = {'zero':  len(os.listdir("data/test/0")),
-             'one':   len(os.listdir("data/test/1")),
-             'two':   len(os.listdir("data/test/2")),
-             'three': len(os.listdir("data/test/3")),
-             'four':  len(os.listdir("data/test/4")),
-             'five':  len(os.listdir("data/test/5"))}
 
 '''
 .............................................................FUNCTION  1............................................
@@ -80,7 +87,7 @@ def returnDifference():                                                         
     else:
         diff = cv2.absdiff(bckimg.astype("uint8"), roi)                      #takes difference with those averaged pics
         diff = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)[1]            #thresholding GRAY img
-        diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernalOpen)            #MORPHING for ignoring small dots
+7        diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernalOpen)            #MORPHING for ignoring small dots
         diff = cv2.morphologyEx(diff, cv2.MORPH_CLOSE, kernalClose)          #filling blank space in hand
         diff = cv2.resize(diff, (350, 350))
         return diff
@@ -91,7 +98,7 @@ def blankScreenDisplay():                                                       
     global mode
     blank_screen = np.zeros((190, 500, 3), np.uint8)
     cv2.putText(blank_screen, 'MODE : ' + mode, (127, 30), font, 2, color[0], 2)
-    cv2.putText(blank_screen, 'NO. OF IMAGES :   train  ||  test', (20, 46), font, 1, color[1], 1)
+    cv2.putText(blank_screen, 'NO. OF IMAGES :   TRAIN  ||  TEST', (20, 46), font, 1, color[1], 1)
     
     cv2.putText(blank_screen, 'HI/BYE', (20, 65), font, 1, color[2], 1)
     cv2.putText(blank_screen, 'THUMBS UP', (20, 85), font, 1, color[2], 1)
@@ -116,61 +123,103 @@ def blankScreenDisplay():                                                       
     cv2.putText(blank_screen, str(trainCount['four']),  (200, 145), font, 1, color[0], 1)
     cv2.putText(blank_screen, str(trainCount['five']),  (200, 165), font, 1, color[0], 1)
     
-    cv2.putText(blank_screen, str(testCount['zero']),   (260, 65), font, 1, color[0], 1)
-    cv2.putText(blank_screen, str(testCount['one']),    (260, 85), font, 1, color[0], 1)
-    cv2.putText(blank_screen, str(testCount['two']),    (260, 105), font, 1, color[0], 1)
-    cv2.putText(blank_screen, str(testCount['three']),  (260, 125), font, 1, color[0], 1)
-    cv2.putText(blank_screen, str(testCount['four']),   (260, 145), font, 1, color[0], 1)
-    cv2.putText(blank_screen, str(testCount['five']),   (260, 165), font, 1, color[0], 1)
+    cv2.putText(blank_screen, str(testCount['zero']),   (270, 65), font, 1, color[0], 1)
+    cv2.putText(blank_screen, str(testCount['one']),    (270, 85), font, 1, color[0], 1)
+    cv2.putText(blank_screen, str(testCount['two']),    (270, 105), font, 1, color[0], 1)
+    cv2.putText(blank_screen, str(testCount['three']),  (270, 125), font, 1, color[0], 1)
+    cv2.putText(blank_screen, str(testCount['four']),   (270, 145), font, 1, color[0], 1)
+    cv2.putText(blank_screen, str(testCount['five']),   (270, 165), font, 1, color[0], 1)
     
     cv2.putText(blank_screen, 'EXIT -->  KEY : ESC', (305, 185), font, 1, color[1], 1)
     cv2.imshow('Info', blank_screen)
     
     
+def checkLength():
+    global trainCount
+    global testCount
+    trainCount = {'zero': len(os.listdir("data/TRAIN/0")),
+                  'one':   len(os.listdir("data/TRAIN/1")),
+                  'two':   len(os.listdir("data/TRAIN/2")),
+                  'three': len(os.listdir("data/TRAIN/3")),
+                  'four':  len(os.listdir("data/TRAIN/4")),
+                  'five':  len(os.listdir("data/TRAIN/5"))}
+
+    testCount = {'zero':  len(os.listdir("data/TEST/0")),
+                 'one':   len(os.listdir("data/TEST/1")),
+                 'two':   len(os.listdir("data/TEST/2")),
+                 'three': len(os.listdir("data/TEST/3")),
+                 'four':  len(os.listdir("data/TEST/4")),
+                 'five':  len(os.listdir("data/TEST/5"))}    
+    
 '''
 ...............................................................MAIN FUNCTION.........................................
-'''    
+'''  
+  
 if __name__ == "__main__":
     mode = 'TRAIN'
     # Create the directory structure
-    if not os.path.exists("data"):
-        os.makedirs("data")
-        os.makedirs("data/train")
-        os.makedirs("data/test")
-        
-        os.makedirs("data/train/0")
-        os.makedirs("data/train/1")
-        os.makedirs("data/train/2")
-        os.makedirs("data/train/3")
-        os.makedirs("data/train/4")
-        os.makedirs("data/train/5")
-        
-        os.makedirs("data/test/0")
-        os.makedirs("data/test/1")
-        os.makedirs("data/test/2")
-        os.makedirs("data/test/3")
-        os.makedirs("data/test/4")
-        os.makedirs("data/test/5")
+    
     while True:
         ret, image = camera.read()
         diffImg = returnDifference()
         
         num_frames += 1        
         
+        checkLength()
         blankScreenDisplay()
         cv2.imshow('original Image', image)
         
         if diffImg is not None:
             cv2.imshow('differenceImage', diffImg)
+            diffImg = cv2.resize(diffImg, (64, 64))
         
         keyPressed = cv2.waitKey(1) & 0xFF                                       #ESC key to terminate and 0xFF for 64-bit computers
+        
         if keyPressed == 27:
             break
+
         elif keyPressed == ord('m'):
             if mode == 'TEST':
                 mode = 'TRAIN'
             else:
                 mode='TEST'
+                
+        elif keyPressed == ord('0'):
+            if mode == 'TEST':
+                cv2.imwrite('data/TEST/0/'+str(testCount['zero'])+'.jpg', diffImg)
+            else:
+                cv2.imwrite('data/TRAIN/0/'+str(trainCount['zero'])+'.jpg', diffImg)
+
+        elif keyPressed == ord('1'):
+            if mode == 'TEST':
+                cv2.imwrite('data/TEST/1/'+str(testCount['one'])+'.jpg', diffImg)
+            else:
+                cv2.imwrite('data/TRAIN/1/'+str(trainCount['one'])+'.jpg', diffImg)
+
+        elif keyPressed == ord('2'):
+            if mode == 'TEST':
+                cv2.imwrite('data/TEST/2/'+str(testCount['two'])+'.jpg', diffImg)
+            else:
+                cv2.imwrite('data/TRAIN/2/'+str(trainCount['two'])+'.jpg', diffImg)
+
+        elif keyPressed == ord('3'):
+            if mode == 'TEST':
+                cv2.imwrite('data/TEST/3/'+str(testCount['three'])+'.jpg', diffImg)
+            else:
+                cv2.imwrite('data/TRAIN/3/'+str(trainCount['three'])+'.jpg', diffImg)
+
+        elif keyPressed == ord('4'):
+            if mode == 'TEST':
+                cv2.imwrite('data/TEST/4/'+str(testCount['four'])+'.jpg', diffImg)
+            else:
+                cv2.imwrite('data/TRAIN/4/'+str(trainCount['four'])+'.jpg', diffImg)
+
+        elif keyPressed == ord('5'):
+            if mode == 'TEST':
+                cv2.imwrite('data/TEST/5/'+str(testCount['five'])+'.jpg', diffImg)
+            else:
+                cv2.imwrite('data/TRAIN/5/'+str(trainCount['five'])+'.jpg', diffImg)
+
         else:
             pass
           
